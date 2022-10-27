cffad86815b72cebbde08a5e875a22ae75d0a5b4
eugli <lieugenebaba01@gmail.com>
2022-10-27 02:48:31 -0400

    frontend changes

--- client/src/App/InfoPanel/ImageSlider/index.js
+++ client/src/App/InfoPanel/ImageSlider/index.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "@mui/material/Slider";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Canvas from "../../Canvas";
 import ImageUpload from "../ImageUpload";
 
@@ -19,10 +20,34 @@ const CustomSlider = styled(Slider)(() => ({
 const ImageSlider = ({ images, width, height }) => {
   const [imageIndex, setImageIndex] = useState(0);
   const [image, setImage] = useState(null);
+  //   const [images, setImages] = useState(initImages);
 
-  const handleUpload = (img) => {
-    console.log(img);
+  useEffect(() => {
+    // console.log(initImages);
+  });
+
+  const handleUpload = async (img) => {
     setImage(img);
+
+    const response = await fetch("http://localhost:8000/magic", {
+      method: "POST",
+      body: "lol,lmao", // string or object
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    const imageSources = myJson[1];
+    const bitMaps = await Promise.all(
+      imageSources.map(async (elem) => {
+        const response = await fetch(elem);
+        const imageBlob = await response.blob();
+        const imagebit = await createImageBitmap(imageBlob);
+        return imagebit;
+      })
+    );
+
+    // setImages(bitMaps);
   };
 
   function drawImageScaled(img, ctx) {
@@ -32,7 +57,7 @@ const ImageSlider = ({ images, width, height }) => {
     var ratio = Math.min(hRatio, vRatio);
     var centerShift_x = (canvas.width - img.width * ratio) / 2;
     var centerShift_y = (canvas.height - img.height * ratio) / 2;
-    
+
     ctx.drawImage(
       img,
       0,
@@ -90,7 +115,7 @@ const ImageSlider = ({ images, width, height }) => {
         </Grid>
 
         <Grid item>
-          <ImageUpload handleUpload={handleUpload}/>
+          <ImageUpload handleUpload={handleUpload} />
         </Grid>
       </Grid>
     </div>
